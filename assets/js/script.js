dd6e7d20ccd30d63c95ad9cf874ccd4366293679
moeinparvizi <giventulip@gmail.com>
2023-09-01 18:17:21 +0330

    movement in snake game

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const playboard = document.querySelector('.wrapper-play-board')
 // variables
 let foodY = 0, foodX = 0
 let snakeY = 5, snakeX = 5
+let velocityX = 0, velocityY = 0
 
 // create random position snake food (apple)
 const randomFood = () => {
@@ -15,12 +16,51 @@ const randomFood = () => {
   foodY = parseInt(foodY)
 }
 
+// change direction of snake
+const changeDirection = (e) => {
+  // let snake = document.querySelector('.play-board-snake')
+  switch (e.key) {
+    case "ArrowUp":
+      velocityX = 0
+      velocityY = -1
+      // snake.style.transform = 'rotate(90deg)'
+      break;
+
+    case "ArrowDown":
+      velocityX = 0
+      velocityY = 1
+      break;
+
+    case "ArrowLeft":
+      velocityX = -1
+      velocityY = 0
+      break;
+
+    case "ArrowRight":
+      velocityX = 1
+      velocityY = 0
+      break;
+  }
+  initGame()
+}
+
 // main function
 const initGame = () => {
-  randomFood()
   // create food in playboard
   let htmlMarkup = `<div class="play-board-food" style="grid-area: ${foodY} / ${foodX}"></div>`
+
+  // update location of snake
+  snakeY += velocityY
+  snakeX += velocityX
+
+  // create snake in map
   htmlMarkup += `<div class="play-board-snake" style="grid-area: ${snakeY} / ${snakeX}"></div>`
+
+  // inner all Element in playboard
   playboard.innerHTML = htmlMarkup
 }
+randomFood()
 initGame()
+
+// click to start
+document.addEventListener('keydown', changeDirection)
