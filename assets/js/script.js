6f8be599cbd4a016cb3099b2ca01579419fc7ea3
moeinparvizi <giventulip@gmail.com>
2023-09-01 19:07:25 +0330

    when sanke eat an apple add dome div to body

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,15 @@ const playboard = document.querySelector('.wrapper-play-board')
 // variables
 let foodY = 0, foodX = 0
 let snakeY = 5, snakeX = 5
+let snakeBody = []
 let velocityX = 0, velocityY = 0
 
 // create random position snake food (apple)
 const randomFood = () => {
   // create random 1 to 30
-  foodX = Math.random() * 30
-  foodY = Math.random() * 30
+  foodX = Math.floor(Math.random() * 30) + 1
+  foodY = Math.floor(Math.random() * 30) + 1
+  console.log(foodX)
   // create number to integer
   foodX = parseInt(foodX)
   foodY = parseInt(foodY)
@@ -51,17 +53,31 @@ const initGame = () => {
 
   //eat foot
   if (snakeX == foodX && snakeY == foodY) {
-
     // change food position after eat an apple
     randomFood()
+
+    // add to body
+    snakeBody.push([foodX, foodY])
+    console.log(snakeBody)
   }
+
+  // shifting forward the values of the elemens in the snake body by one
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1]
+  }
+
+  // setting first element of snake body to current snake position
+  snakeBody[0] = [snakeX, snakeY]
 
   // update location of snake
   snakeY += velocityY
   snakeX += velocityX
 
-  // create snake in map
-  htmlMarkup += `<div class="play-board-snake" style="grid-area: ${snakeY} / ${snakeX}"></div>`
+  // adding a div for each part of the snake's body
+  for (let i = 0; i < snakeBody.length; i++) {
+    // create snake in map
+    htmlMarkup += `<div class="play-board-snake" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`
+  }
 
   // inner all Element in playboard
   playboard.innerHTML = htmlMarkup
