04294a60e8a06db73f8815f02ec610fba4c3add5
moeinparvizi <giventulip@gmail.com>
2023-09-01 17:11:02 +0330

    create random apple on playboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,22 @@
 const playboard = document.querySelector('.wrapper-play-board')
 
 // snke food (apple) location
-let foodX = 13
-let foodY = 10
+// let foodX = 13
+// let foodY = 10
+
+// create random snake food
+const randomFood = () => {
+  // create random 1 to 30
+  foodX = Math.random() * 30
+  foodY = Math.random() * 30
+  // create number to integer
+  foodX = parseInt(foodX)
+  foodY = parseInt(foodY)
+}
 
 // main function
-
 const initGame = () => {
+  randomFood()
   // create food in playboard
   let foodMarkup = `<div class="play-board-food" style="grid-area: ${foodY} / ${foodX}"></div>`
   playboard.innerHTML = foodMarkup
