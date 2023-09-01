ffb259e4c1d4bb20a144fdc8a2083ea24b3257f9
moeinparvizi <giventulip@gmail.com>
2023-09-01 16:57:36 +0330

    added food to snake game and add apple picture to grid area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log('hello test')
+// DOM 
+const playboard = document.querySelector('.wrapper-play-board')
+
+// snke food (apple) location
+let foodX = 13
+let foodY = 10
+
+// main function
+
+const initGame = () => {
+  // create food in playboard
+  let foodMarkup = `<div class="play-board-food" style="grid-area: ${foodY} / ${foodX}"></div>`
+  playboard.innerHTML = foodMarkup
+}
+initGame()
